e0e17f445e4e695e090343c0956682074011f945
Info Toonz <111harisawan@gmail.com>
2025-10-01 15:04:33 +0000

    updated

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -15,7 +15,7 @@
   services.firebase.emulators = {
     # Disabling because we are using prod backends right now
     detect = false;
-    projectId = "demo-app";
+    projectId = "rehnumaai-7f4c1";
     services = ["auth" "firestore"];
   };
   idx = {
